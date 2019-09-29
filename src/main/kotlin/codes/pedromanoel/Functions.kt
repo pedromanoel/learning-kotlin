95b3b1a564e0c181e884b582ed58b16306ce3bf6
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2019-09-29 19:29:54 -0300

    Improve code examples readability
    - Put the declarations and usage together
    - Change the example to provide some sort of computationˆ

--- src/main/kotlin/codes/pedromanoel/Functions.kt
+++
@@ -1,9 +0,0 @@
-package codes.pedromanoel
-
-fun helloWorld(): String {
-    return "Hello World complete declaration"
-}
-
-fun helloWorldShortExplicitReturn(): String = "Hello World explicit return"
-
-fun helloWorldShortImplicitReturn() = "Hello World implicit return"
--- src/test/kotlin/codes/pedromanoel/FunctionsTest.kt
+++ src/test/kotlin/codes/pedromanoel/FunctionsTest.kt
@@ -7,12 +7,44 @@ import kotlin.test.Test
 class FunctionsTest {
 
     @Test
-    fun function_declarations() {
-        assertThat(helloWorld())
-            .isEqualTo("Hello World complete declaration")
-        assertThat(helloWorldShortExplicitReturn())
-            .isEqualTo("Hello World explicit return")
-        assertThat(helloWorldShortImplicitReturn())
-            .isEqualTo("Hello World implicit return")
+    fun different_function_declarations() {
+        assertThat(upper1("complete")).isEqualTo("COMPLETE")
+
+        assertThat(upper2("short")).isEqualTo("SHORT")
+
+        assertThat(upper3("implicit return")).isEqualTo("IMPLICIT RETURN")
+
+        assertThat(upper4("lambda")).isEqualTo("LAMBDA")
     }
+
+    private fun upper1(value: String): String {
+        return value.toUpperCase()
+    }
+
+    private fun upper2(value: String): String = value.toUpperCase()
+
+    private fun upper3(value: String) = value.toUpperCase()
+
+    val upper4 = { value: String -> value.toUpperCase() }
+
+    @Test
+    fun function_with_default_parameters() {
+        val result1 = printPersonDetails()
+        val result2 = printPersonDetails("Pedro", 35)
+
+        assertThat(result1).isEqualTo("Name: <UNDEFINED>, Age: <UNDEFINED>")
+        assertThat(result2).isEqualTo("Name: Pedro, Age: 35")
+    }
+
+    @Test
+    fun named_parameters() {
+        val result = printPersonDetails(age = 35, name = "Pedro")
+
+        assertThat(result).isEqualTo("Name: Pedro, Age: 35")
+    }
+
+    private fun printPersonDetails(
+        name: String = "<UNDEFINED>",
+        age: Int = 0
+    ) = "Name: $name, Age: ${if (age == 0) "<UNDEFINED>" else age.toString()}"
 }
